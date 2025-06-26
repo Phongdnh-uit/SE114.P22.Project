5de04f11f10e051c897bbee77aa9dd1128693dd5
Gnuttttt <dinhthanhtung0312@gmail.com>
2025-06-26 11:04:42 +0700

    Done UI

--- frontend/MAM/app/src/main/java/com/example/mam/dto/review/ReviewResponse.kt
+++ frontend/MAM/app/src/main/java/com/example/mam/dto/review/ReviewResponse.kt
@@ -4,8 +4,8 @@ import com.example.mam.dto.BaseResponse
 import java.time.Instant
 
 data class ReviewResponse(
-    val orderId: Long,
-    val rate: Int,
+    val orderId: Long = 0L,
+    val rate: Int = 0,
     val content: String? = null,
     val reply: String? = null,
 ): BaseResponse()
