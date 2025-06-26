894a02c17c508678533c143df62e4f7e17d41d73
CaHoiAu <trantranchau1605@gmail.com>
2025-06-26 12:58:15 +0700

    Sửa otp

--- frontend/MAM/app/build.gradle.kts
+++ frontend/MAM/app/build.gradle.kts
@@ -79,5 +79,5 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("co.yml:ycharts:2.1.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 }
